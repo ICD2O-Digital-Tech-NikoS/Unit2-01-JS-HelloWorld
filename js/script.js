3d8a858dc7a6c9e771821f8043abf0c6b30a5452
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-03-06 21:43:22 +0000

    u

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Ms Raffin
+// Created by: Ms Raffin, edited by: Niko
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
   // this function displays "Hello, World!"
 
-  document.getElementById("hello-world").innerHTML = "I coded this in Javascript!"
+  document.getElementById("button-hello-world").innerHTML = "I coded this in Javascript!"
 }
